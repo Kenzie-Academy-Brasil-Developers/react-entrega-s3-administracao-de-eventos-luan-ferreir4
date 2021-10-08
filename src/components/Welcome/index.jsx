a93381f98778503bf1143fbad8d88985c70e6d91
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-08 16:42:57 -0300

    feat: landing page main text

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,10 @@
+export const Welcome = () => {
+    return(
+        <section>
+            <h3>Drinks</h3>
+            <img src="images/divider.png" alt="divider" />
+            <p>O gerenciador de bebidas</p>
+            <p>para grandes eventos</p>
+        </section>
+    )
+}
