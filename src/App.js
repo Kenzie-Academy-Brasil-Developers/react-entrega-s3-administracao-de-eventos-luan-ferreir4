bfc15cd27ff99547b54425823f79a92d06431a0d
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-07 22:09:17 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+const App = () => {
+  return (
+    <>
+      <header>
+        
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </>
+  );
+}
+
+export default App;
