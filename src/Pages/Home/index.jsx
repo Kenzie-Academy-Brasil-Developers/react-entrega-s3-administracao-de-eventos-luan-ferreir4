8e12ffda6960eb32f5937c5155b79265abeebd4e
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-08 16:45:39 -0300

    refactor: imported styled components

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,19 @@
+import { Button } from "../../components/Button";
+import { Welcome } from "../../components/Welcome";
+import { Header, Main } from "../../styles/landingPage";
+
 const Home = () => {
-    return(
-        <>
-        Home
-        </>
-    )    
-}
+  return (
+    <>
+      <Header>
+          <img src="images/bottleWhite.png" alt="wine-bottle" />
+      </Header>
+      
+      <Main>
+        <Welcome />
+        <Button type="initial"/>
+      </Main>
+    </>
+  );
+};
 export default Home;
