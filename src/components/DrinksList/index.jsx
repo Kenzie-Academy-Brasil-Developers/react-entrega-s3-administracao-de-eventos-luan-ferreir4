7f6761cc1f587b8e9cd87c6af49af4b0a4f780af
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-08 22:25:48 -0300

    feat: added components for drink list

---
+++ src/components/DrinksList/index.jsx
@@ -0,0 +1,18 @@
+import { DrinkCard } from "../DrinkCard";
+import { Catalogue } from "../../styles/cataloguePage";
+import { useContext } from "react";
+import { DrinksContext } from "../../Providers/AllDrinks";
+
+export const DrinkList = () => {
+  const { drinkList } = useContext(DrinksContext);
+  console.log(drinkList);
+  return (
+    <Catalogue>
+      <h1>Nossas bebidas disponíveis</h1>
+      <img src="images/divider.png" alt="divider" />
+      {drinkList.map((item, index) => (
+        <DrinkCard key={index} drink={item} />
+      ))}
+    </Catalogue>
+  );
+};
