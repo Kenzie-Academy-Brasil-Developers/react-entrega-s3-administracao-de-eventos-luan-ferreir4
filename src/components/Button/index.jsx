d4f768e25672ccfb19e7416fab93e020a73c73dc
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-08 22:27:28 -0300

    refactor: more type options

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
-export const Button = ({ type }) => {
+export const Button = ({ type, setState }) => {
   const history = useHistory();
   const [content, setContent] = useState("");
 
-    useEffect(()=>{
-        if (type === "initial") {
-            setContent("Veja nosso catálogo");
-        }
-        else if (type === "toHome") {
-            setContent("Home");
-        }
-    },[type]);
+  useEffect(() => {
+    if (type === "initial") {
+      setContent("Veja nosso catálogo");
+    } else if (type === "graduation") {
+      setContent("Formatura");
+    } else if (type === "wedding") {
+      setContent("Casamento");
+    } else if (type === "confrat") {
+      setContent("Confraternização");
+    } else if (type === "toEvents") {
+      setContent("Eventos");
+    }
+  }, [type]);
 
   const chooseType = () => {
     if (type === "initial") {
       history.push("/catalogue");
+    } else if (type === "graduation") {
+    } else if (type === "wedding") {
+    } else if (type === "confrat") {
     }
   };
 
