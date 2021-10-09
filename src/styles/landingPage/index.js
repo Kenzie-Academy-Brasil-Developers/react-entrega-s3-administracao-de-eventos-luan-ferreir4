4cc0d9d5f5276678b85b77ff9818d2fa54fdbae7
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-09 18:44:37 -0300

    style: ladding page button

--- src/styles/landingPage/index.js
+++ src/styles/landingPage/index.js
@@ -36,7 +36,7 @@ export const Main = styled.main`
     width: 90vw;
     margin: 8vh auto 0;
     h3 {
-      margin-bottom: 0px;
+      margin: 45px 0 0px;
       font-weight: 400;
       font-size: 4.8rem;
     }
@@ -49,6 +49,21 @@ export const Main = styled.main`
       font-weight: 100px;
       font-size: 1.5rem;
       margin: 5px 0 0px;
+    }
+
+  }
+  Button {
+    padding: 12px 20px;
+    margin: 20px;
+    background-color: #595959;
+    color: #f5f5f5;
+    border: 1px solid transparent;
+    border-radius: 5px;
+    font-size: 1.2rem;
+    
+    &:hover {
+      background-color: #f5f5f5;
+      color: #7e7e7e;
     }
   }
 `;
