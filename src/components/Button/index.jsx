6b10b38ac23ae103b9171921e2ec6532a29ab6e0
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-10 01:45:08 -0300

    refactor: removing navigation features

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,34 +5,26 @@ import { WeddingContext } from "../../Providers/WeddingDrinks";
 import { ConfratContext } from "../../Providers/ConfratDrinks";
 
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
 
 export const Button = ({ type, drinkId }) => {
-  const history = useHistory();
   const [content, setContent] = useState("");
 
   const { addToGraduation } = useContext(GraduationContext);
   const { addToWedding } = useContext(WeddingContext);
-  const { addToConfrat } = useContext(ConfratContext)
+  const { addToConfrat } = useContext(ConfratContext);
 
   useEffect(() => {
-    if (type === "initial") {
-      setContent("Veja nosso catálogo");
-    } else if (type === "graduation") {
+    if (type === "graduation") {
       setContent("Formatura");
     } else if (type === "wedding") {
       setContent("Casamento");
     } else if (type === "confrat") {
       setContent("Confraternização");
-    } else if (type === "toEvents") {
-      setContent("Eventos");
     }
   }, [type]);
 
   const chooseType = () => {
-    if (type === "initial") {
-      history.push("/catalogue");
-    } else if (type === "graduation") {
+    if (type === "graduation") {
       addToGraduation(drinkId);
     } else if (type === "wedding") {
       addToWedding(drinkId);
