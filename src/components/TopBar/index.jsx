12108d532cb98b35303e027d586cb180a94d8737
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-10 13:56:42 -0300

    Revert "refactor: dividing TopBar style in a new folder"
    This reverts commit d0510e9bc92dbefef818d74570fc8a92ead9053b.

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from "react";
 
-<<<<<<< HEAD
 import { Bar } from "../../styles/topBar";
-=======
-import { Bar } from "../../styles/cataloguePage";
->>>>>>> parent of d0510e9 (refactor: dividing TopBar style in a new folder)
 
 import { ButtonNav } from "../ButtonNav";
 
