1c4333b66348408825e9e705188b1767ff7885c2
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-11 00:01:49 -0300

    style: basic style for visualization

---
+++ src/styles/eventDrinks/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const EventDinkList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 94vw;
+  height: 84vh;
+  margin: 14vh auto 0;
+  background-color: #f5f5f5;
+  box-shadow: 0px 2px 7px #595959;
+
+  h1{
+    font-weight: 300;
+    margin-bottom: 0;
+  }
+
+  img {
+    width: 80vw;
+    max-width: 300px;
+  }
+`;
