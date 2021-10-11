1e02075647b8b1f69d75f87a9c54c8cdcd2094a9
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-11 16:12:32 -0300

    feat: button to remove drinks from lists

---
+++ src/components/ButtonRemove/index.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { GraduationContext } from "../../Providers/GraduationDrinks";
+import { WeddingContext } from "../../Providers/WeddingDrinks";
+import { ConfratContext } from "../../Providers/ConfratDrinks";
+
+export const ButtonRemove = ({ eventType, drinkId }) => {
+  const { removeFromGrad } = useContext(GraduationContext);
+  const { removeFromWed } = useContext(WeddingContext);
+  const { removeFromConf } = useContext(ConfratContext);
+
+  const chooseAction = () => {
+      if (eventType === "grad") {
+          removeFromGrad(drinkId);
+      }
+      else if (eventType === "wed") {
+        removeFromWed(drinkId)
+      }
+      else if (eventType === "conf") {
+        removeFromConf(drinkId)
+      }
+  };
+
+  return <button onClick={chooseAction}>Remover</button>;
+};
