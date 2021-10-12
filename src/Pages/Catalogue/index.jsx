6438d565e55cd42305f4732c5de98148b8957510
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-12 02:14:54 -0300

    refactor: imported alert div

--- src/Pages/Catalogue/index.jsx
+++ src/Pages/Catalogue/index.jsx
@@ -1,9 +1,23 @@
+import { useContext } from "react/cjs/react.development";
+import { AlertsContext } from "../../Providers/Alerts";
+import { AlertCard } from "../../styles/alertCard";
 import { TopBar } from "../../components/TopBar";
 import { DrinkList } from "../../components/DrinksList";
 
 const Catalogue = () => {
+
+    const { added } = useContext(AlertsContext);
+    
+    const { already } = useContext(AlertsContext);
+
     return(
         <>
+        {added &&
+            <AlertCard style={{backgroundColor: "#7BBE5C", top: "14%"}}>Added drink</AlertCard>
+        }
+        {already &&
+            <AlertCard  style={{backgroundColor: "#f4735d", top: "20%"}}>Already added to this event</AlertCard>
+        }
         <TopBar type="catalogue"/>
         <DrinkList/>
         </>
