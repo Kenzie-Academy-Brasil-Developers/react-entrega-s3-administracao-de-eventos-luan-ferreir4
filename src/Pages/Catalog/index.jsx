344b05a64f10a92108c5a42be62ebf53da20a107
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-10-12 03:58:38 -0300

    fix: import path

--- src/Pages/Catalog/index.jsx
+++ src/Pages/Catalog/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import { AlertsContext } from "../../Providers/Alerts";
 import { AlertCard } from "../../styles/alertCard";
 import { TopBar } from "../../components/TopBar";
